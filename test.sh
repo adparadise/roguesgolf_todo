debb26593765a8bf5a22a13f1bea65e8dab3ea1d
Andrew Paradise <andrew@cantinaconsulting.com>
2012-05-16 12:30:22 -0400

    minified to 222 characters

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 rm .t
 rm output.txt
-./todo.rb thing1 >> output.txt &&\
-./todo.rb thing2 >> output.txt &&\
-./todo.rb >> output.txt &&\
-./todo.rb 1 >> output.txt &&\
-./todo.rb >> output.txt 
+ruby ./todo.rb thing1 >> output.txt &&\
+ruby ./todo.rb thing2 >> output.txt &&\
+ruby ./todo.rb >> output.txt &&\
+ruby ./todo.rb 1 >> output.txt &&\
+ruby ./todo.rb >> output.txt 
 echo "" >> output.txt
 cat .t >> output.txt
 

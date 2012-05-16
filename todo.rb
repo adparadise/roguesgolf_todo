006e11651389f5493a12bd77479fc660446c602b
Andrew Paradise <andrew@cantinaconsulting.com>
2012-05-16 16:53:05 -0400

    add ruby 1.8.7 support: 181 chars

--- todo.rb
+++ todo.rb
@@ -1,2 +1,2 @@
-c=$*.join " ";c,d=c=~/^\d+$/?["",c.to_i]:c;`touch .t`;r=IO.readlines ".t";c==''||r<<c+"\n";i=1;IO.write ".t",r.map{|l|l[0]=="#"||(i!=d||l="# "+l;$><<"#{i} "+l;i+=1);l}.join
+c=$*.join " ";c,d=c=~/^\d+$/?["",c.to_i]:c;`touch .t`;r=IO.readlines ".t";c==""||r<<c+"\n";i=1;File.new(".t","w")<<r.map{|l|l[0..0]=="#"||(i!=d||l="# "+l;$><<"#{i} "+l;i+=1);l}.join
 #                                                                                                                                            |=====

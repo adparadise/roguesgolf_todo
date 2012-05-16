b01dd867dce30c76e3d61ee9b896bc61d8ab3dc5
Andrew Paradise <andrew@cantinaconsulting.com>
2012-05-16 13:31:18 -0400

    reduce length to 196

--- todo.rb
+++ todo.rb
@@ -1,1 +1,2 @@
-c=ARGV.join(" ").strip;`touch .t`;r=File.readlines(".t");(c,d)=(c=~/^\d+$/)?["",$~[0].to_i]:[c,nil];r<<(c+"\n")if(c.size>0);i=1;File.write(".t",(r.map {|l|(l=~/^\#/)||(l=(i==d)?"# #{l}":l;puts "#{i} #{l}";i+=1);l}.join()))
+c=ARGV.join " ";`touch .t`;r=File.readlines ".t";c,d=c=~/^\d+$/?["",$~[0].to_i]:c;r<<c+"\n"if c.size>0;i=1;File.write ".t",r.map {|l|(l=~/^\#/)||(l=(i==d)?"# #{l}":l;puts "#{i} #{l}";i+=1);l}.join
+#                                                                                                                                            |=====

482210c44b4af96542c86307e296cde94f50aff2
Andrew Paradise <andrew@cantinaconsulting.com>
2012-05-16 12:04:26 -0400

    fairly minified

--- todo.rb
+++ todo.rb
@@ -1,17 +1,5 @@
 #!/usr/bin/env ruby
-`touch .t`
-r = File.readlines(".t")
-c=ARGV.join(" ").strip
-(c,d)=(c=~/^\d+$/)?["",$~[0].to_i]:[c,nil]
-r<<(c+"\n") if (c.length > 0)
-c=c.length>0?c+"\n":nil
-i=1
-File.write(".t",(r.map do |l|
-                   if !(l=~/^\#/)
-                     l = "# #{l}" if i == d
-                     puts "#{i} #{l}"
-                     i += 1
-                   end
-                   l
-                 end.join()))
-           
+`touch .t`;r=File.readlines(".t");c=ARGV.join(" ").strip;(c,d)=(c=~/^\d+$/)?["",$~[0].to_i]:[c,nil];r<<(c+"\n")if(c.length > 0);i=1
+File.write(".t",(r.map {|l|if !(l=~/^\#/)
+                             l=(i==d)?"# #{l}":l;puts "#{i} #{l}";i+=1
+                           end;l}.join()))

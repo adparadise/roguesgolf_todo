33284b93d0a15914bc34fd2a158ec24dde3234da
Andrew Paradise <andrew@cantinaconsulting.com>
2012-05-16 14:16:37 -0400

    down to 177 characters

--- todo.rb
+++ todo.rb
@@ -1,2 +1,2 @@
-c=ARGV.join " ";`touch .t`;r=File.readlines ".t";c,d=c=~/^\d+$/?["",$~[0].to_i]:c;r<<c+"\n"if c.size>0;i=1;File.write ".t",r.map {|l|(l=~/^\#/)||(l=(i==d)?"# #{l}":l;puts "#{i} #{l}";i+=1);l}.join
+c=$*.join " ";`touch .t`;r=IO.readlines ".t";c,d=c=~/^\d+$/?["",c.to_i]:c;c==''||r<<c+"\n";i=1;IO.write ".t",r.map{|l|l[0]=="#"||(l=(i==d)?"# #{l}":l;$><<"#{i} "+l;i+=1);l}.join
 #                                                                                                                                            |=====
